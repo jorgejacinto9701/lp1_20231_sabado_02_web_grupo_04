7392f8e947bcfffd0d58993f939ffc5ba815bf1e
RaulFloresHzo <ra_070@hotmail.com>
2023-05-24 02:00:12 -0500

    Crud Autor: Al darle al filtro  no me bota la lista

--- src/main/java/dao/AutorDAO.java
+++ src/main/java/dao/AutorDAO.java
@@ -1,11 +1,21 @@
 package dao;
 
+import java.sql.Date;
+import java.util.List;
+
 import entity.Autor;
+
+
 
 public interface AutorDAO {
 	
+	public abstract List<Autor> listaPorFecha(Date fecInicio, Date fecFin);
+	
 	public abstract int insertaAutor(Autor obj);
-
+	public abstract List<Autor> listaAutor(String filtro);
+	public abstract int actualizaAutor(Autor obj);
+	public abstract int eliminaAutor(int idAutor);
+	public abstract Autor buscaAutor(int idAutor);
 	
 }
 
